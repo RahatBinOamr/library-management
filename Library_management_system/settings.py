feae8a424b440cc7d2cd9bcf427df661eec9f17a
RahatBinOamr <108364543+RahatBinOamr@users.noreply.github.com>
2024-08-26 18:50:57 +0600

    chagne database with mysql

--- Library_management_system/settings.py
+++ Library_management_system/settings.py
@@ -72,8 +72,12 @@ WSGI_APPLICATION = 'Library_management_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'library_management_system',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
